b5dd6c090a56bc548996944cc16fd56633a4bd25
Ferris <ferris.oss1@gmail.com>
2021-09-25 09:01:50 +0530

    initial commit

---
+++ Lol-learning.sh
@@ -0,0 +1,11 @@
+#!/usr/local/bin/lci
+HAI 1.2
+I HAS A fi ITZ A NUMBR 
+I HAS A si ITZ A NUMBR
+VISIBLE "enter first integer: "
+GIMMEH fi
+VISIBLE "enter second integer: "
+GIMMEH si
+VISIBLE "the sum is"
+VISIBLE SUM OF fi AN si
+KTHXBYE
